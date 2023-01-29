858fb34fce930cea0de7c7909fd467229b89592d
houtaroy <houtaroy@gmail.com>
2023-01-29 16:59:01 +0800

    feat(MyBatis): 废弃愚蠢的冗余配置

--- koala-starters/koala-mybatis-starter/src/main/java/cn/koala/mybatis/autoconfigure/MyBatisAutoConfiguration.java
+++ koala-starters/koala-mybatis-starter/src/main/java/cn/koala/mybatis/autoconfigure/MyBatisAutoConfiguration.java
@@ -1,11 +1,9 @@
 package cn.koala.mybatis.autoconfigure;
 
 import cn.koala.mybatis.handlers.EnhancedEnumTypeHandler;
-import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,7 +15,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @AutoConfigureAfter(MybatisAutoConfiguration.class)
-@EnableConfigurationProperties(MyBatisProperties.class)
 @EnableTransactionManagement
 public class MyBatisAutoConfiguration {
   /**
@@ -28,19 +25,5 @@ public class MyBatisAutoConfiguration {
   @Bean
   public ConfigurationCustomizer enhancedEnumTypeConfigurationCustomizer() {
     return configuration -> configuration.getTypeHandlerRegistry().register(EnhancedEnumTypeHandler.class);
-  }
-
-  /**
-   * MyBatis TypeHandler自定义配置
-   *
-   * @param properties MyBatis配置
-   * @return 自定义配置
-   */
-  @Bean
-  public ConfigurationCustomizer typeHandlerConfigurationCustomizer(MyBatisProperties properties) {
-    return configuration -> {
-      TypeHandlerRegistry registry = configuration.getTypeHandlerRegistry();
-      properties.getTypeHandlerPackages().forEach(registry::register);
-    };
   }
 }
