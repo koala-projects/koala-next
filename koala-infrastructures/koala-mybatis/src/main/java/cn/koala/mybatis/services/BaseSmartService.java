259da0fb80a749f9056e3c99620ed169aa26d5fb
houtaroy <houtaroy@gmail.com>
2023-01-29 09:17:16 +0800

    feat(MyBatis): 基础智能服务类增加系统数据检测

--- koala-infrastructures/koala-mybatis/src/main/java/cn/koala/mybatis/services/BaseSmartService.java
+++ koala-infrastructures/koala-mybatis/src/main/java/cn/koala/mybatis/services/BaseSmartService.java
@@ -1,8 +1,11 @@
 package cn.koala.mybatis.services;
 
 import cn.koala.mybatis.IdModel;
+import cn.koala.mybatis.SystemModel;
+import cn.koala.mybatis.YesNo;
 import cn.koala.mybatis.repositories.BaseRepository;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -11,7 +14,7 @@ import java.util.function.Function;
 /**
  * 基础智能服务抽象类
  * <p>
- * 在基础服务类上新增了部分功能: 代码生成ID/数据存在检查/数据操作校验
+ * 在基础服务类上新增了部分功能: 代码生成ID/数据存在检查/系统数据不可修改/数据操作校验
  *
  * @author Houtaroy
  */
@@ -34,6 +37,8 @@ public abstract class BaseSmartService<T extends IdModel<ID>, ID> extends BaseSe
     Optional<T> persist = repository.findById(entity.getId());
     if (persist.isEmpty()) {
       entity.setIdIfAbsent(idBuilder.apply(entity));
+    } else {
+      Assert.isTrue(isNotSystem(persist.get()), "系统数据不允许修改");
     }
     Function<T, Integer> operation = persist.isEmpty() ? repository::add : repository::update;
     if (operation.apply(entity) != 1) {
@@ -43,9 +48,22 @@ public abstract class BaseSmartService<T extends IdModel<ID>, ID> extends BaseSe
 
   @Override
   public <S extends T> void delete(S entity) {
-    findById(entity.getId());
+    Assert.isTrue(isNotSystem(findById(entity.getId())), "系统数据不允许修改");
     if (repository.delete(entity) != 1) {
       throw new IllegalStateException("数据处理异常");
     }
+  }
+
+  /**
+   * 是否非系统数据
+   *
+   * @param entity 数据实体
+   * @return 是否非系统数据
+   */
+  protected <S extends T> boolean isNotSystem(S entity) {
+    if (entity instanceof SystemModel model) {
+      return model.getIsSystem() == YesNo.NO;
+    }
+    return true;
   }
 }
