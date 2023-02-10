dac59dfdc0d3c70ede448843a522355deef84f81
houtaroy <houtaroy@gmail.com>
2023-02-10 18:07:31 +0800

    feat(系统管理): 增加字典项管理

--- koala-domains/koala-system/src/main/java/cn/koala/system/DictionaryItem.java
+++ koala-domains/koala-system/src/main/java/cn/koala/system/DictionaryItem.java
@@ -1,11 +1,16 @@
 package cn.koala.system;
 
+import cn.koala.mybatis.AuditModel;
+import cn.koala.mybatis.IdModel;
+import cn.koala.mybatis.SortModel;
+import cn.koala.mybatis.StateModel;
+
 /**
  * 字典项接口
  *
  * @author Houtaroy
  */
-public interface DictionaryItem {
+public interface DictionaryItem extends IdModel<Long>, SortModel, StateModel, AuditModel<Long> {
   String getCode();
 
   String getName();
