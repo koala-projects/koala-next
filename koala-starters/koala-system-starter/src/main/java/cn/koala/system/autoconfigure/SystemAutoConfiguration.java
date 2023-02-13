e38cdcd40ae6dec3760da7955ac91e872bc8663b
houtaroy <houtaroy@gmail.com>
2023-02-13 12:02:30 +0800

    feat(系统管理): 增加字典项接口

--- koala-starters/koala-system-starter/src/main/java/cn/koala/system/autoconfigure/SystemAutoConfiguration.java
+++ koala-starters/koala-system-starter/src/main/java/cn/koala/system/autoconfigure/SystemAutoConfiguration.java
@@ -2,15 +2,21 @@ package cn.koala.system.autoconfigure;
 
 import cn.koala.system.apis.DictionaryApi;
 import cn.koala.system.apis.DictionaryApiImpl;
+import cn.koala.system.apis.DictionaryItemApi;
+import cn.koala.system.apis.DictionaryItemApiImpl;
 import cn.koala.system.apis.UserApi;
 import cn.koala.system.apis.UserApiImpl;
+import cn.koala.system.repositories.DictionaryItemRepository;
 import cn.koala.system.repositories.DictionaryRepository;
 import cn.koala.system.repositories.UserRepository;
+import cn.koala.system.services.DictionaryItemService;
+import cn.koala.system.services.DictionaryItemServiceImpl;
 import cn.koala.system.services.DictionaryService;
 import cn.koala.system.services.DictionaryServiceImpl;
 import cn.koala.system.services.UserService;
 import cn.koala.system.services.UserServiceImpl;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -33,22 +39,38 @@ public class SystemAutoConfiguration {
   }
 
   @Bean
+  @ConditionalOnMissingBean
   public UserService userService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
     return new UserServiceImpl(userRepository, passwordEncoder);
   }
 
   @Bean
+  @ConditionalOnMissingBean
   public UserApi userApi(UserService userService) {
     return new UserApiImpl(userService);
   }
 
   @Bean
+  @ConditionalOnMissingBean
   public DictionaryService dictionaryService(DictionaryRepository dictionaryRepository) {
     return new DictionaryServiceImpl(dictionaryRepository);
   }
 
   @Bean
+  @ConditionalOnMissingBean
   public DictionaryApi dictionaryApi(DictionaryService dictionaryService) {
     return new DictionaryApiImpl(dictionaryService);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public DictionaryItemService dictionaryItemService(DictionaryItemRepository dictionaryItemRepository) {
+    return new DictionaryItemServiceImpl(dictionaryItemRepository);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public DictionaryItemApi dictionaryItemApi(DictionaryItemService dictionaryItemService) {
+    return new DictionaryItemApiImpl(dictionaryItemService);
   }
 }
